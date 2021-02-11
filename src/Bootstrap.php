99bda14d5134eb1f48bcc795f2007822557603a6
Rik Meijer <rik@pulledbits.org>
2021-02-11 10:43:22 +0100

    support context variables in configuration loader so relative paths are possible. Also pass a schema so defaults can be loaded
    Signed-off-by: Rik Meijer <rik@pulledbits.org>

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -7,61 +7,22 @@ use ReflectionException;
 use ReflectionFunction;
 use ReflectionParameter;
 
-/**
- * array_merge_recursive does indeed merge arrays, but it converts values with duplicate
- * keys to arrays rather than overwriting the value in the first array with the duplicate
- * value in the second array, as array_merge does. I.e., with array_merge_recursive,
- * this happens (documented behavior):
- *
- * array_merge_recursive(array('key' => 'org value'), array('key' => 'new value'));
- *     => array('key' => array('org value', 'new value'));
- *
- * array_merge_recursive_distinct does not change the datatypes of the values in the arrays.
- * Matching keys' values in the second array overwrite those in the first array, as is the
- * case with array_merge, i.e.:
- *
- * array_merge_recursive_distinct(array('key' => 'org value'), array('key' => 'new value'));
- *     => array('key' => array('new value'));
- *
- * Parameters are passed by reference, though only for performance reasons. They're not
- * altered by this function.
- *
- * @param array $array1
- * @param array $array2
- * @return array
- * @author Daniel <daniel (at) danielsmedegaardbuus (dot) dk>
- * @author Gabriel Sobrinho <gabriel (dot) sobrinho (at) gmail (dot) com>
- */
-function array_merge_recursive_distinct(array $array1, array $array2): array
-{
-    $merged = $array1;
-
-    foreach ($array2 as $key => &$value) {
-        if (is_array($value) && isset ($merged [$key]) && is_array($merged [$key])) {
-            $merged [$key] = array_merge_recursive_distinct($merged [$key], $value);
-        } else {
-            $merged [$key] = $value;
-        }
-    }
-
-    return $merged;
-}
-
 
 final class Bootstrap
 {
     public static function load(string $configurationPath): Closure
     {
-        $path = function () use ($configurationPath): string {
-            $configuration = Configuration::open($configurationPath, ('BOOTSTRAP'));
-            if (array_key_exists('path', $configuration)) {
-                return $configuration['path'];
-            }
-            return $configurationPath . DIRECTORY_SEPARATOR . 'bootstrap';
+        $config = static function (string $section, array $schema) use ($configurationPath) {
+            return Configuration::open($configurationPath, $section, $schema);
+        };
+
+        $path = static function () use ($config): string {
+            $configuration = $config('BOOTSTRAP', ['path' => ['url', ['default' => '%configuration-path%' . DIRECTORY_SEPARATOR . 'bootstrap']]]);
+            return $configuration['path'];
         };
 
         $resources = [];
-        return $bootstrap = function (string $identifier) use (&$bootstrap, $configurationPath, $path, &$resources) {
+        $bootstrap = static function (string $identifier) use (&$bootstrap, $configurationPath, $path, &$resources) {
             if (array_key_exists($identifier, $resources)) {
                 return $resources[$identifier];
             }
@@ -90,7 +51,7 @@ final class Bootstrap
                 if ($reflection->getNumberOfParameters() > 0) {
                     $firstParameter = $reflection->getParameters()[0];
                     if ($resourceRequiresConfigurationParameter($firstParameter)) {
-                        $arguments[$firstParameter->getName()] = Configuration::open($configurationPath, ($identifier));
+                        $arguments[$firstParameter->getName()] = Configuration::open($configurationPath, $identifier, []);
                     }
 
                     if ($reflection->getNumberOfParameters() > count($arguments)) { // multiple parameters
@@ -107,10 +68,12 @@ final class Bootstrap
                     }
                 }
             } catch (ReflectionException $e) {
-
+                trigger_error($e->getMessage());
             }
 
             return $resources[$identifier] = $resource(...$arguments);
         };
+
+        return $bootstrap;
     }
 }
--- src/Configuration.php
+++ src/Configuration.php
@@ -1,22 +1,76 @@
 <?php declare(strict_types=1);
 
-
 namespace rikmeijer\Bootstrap;
 
+/**
+ * array_merge_recursive does indeed merge arrays, but it converts values with duplicate
+ * keys to arrays rather than overwriting the value in the first array with the duplicate
+ * value in the second array, as array_merge does. I.e., with array_merge_recursive,
+ * this happens (documented behavior):
+ *
+ * array_merge_recursive(array('key' => 'org value'), array('key' => 'new value'));
+ *     => array('key' => array('org value', 'new value'));
+ *
+ * array_merge_recursive_distinct does not change the datatypes of the values in the arrays.
+ * Matching keys' values in the second array overwrite those in the first array, as is the
+ * case with array_merge, i.e.:
+ *
+ * array_merge_recursive_distinct(array('key' => 'org value'), array('key' => 'new value'));
+ *     => array('key' => array('new value'));
+ *
+ * Parameters are passed by reference, though only for performance reasons. They're not
+ * altered by this function.
+ *
+ * @param array $array1
+ * @param array ...$arrays
+ * @return array
+ * @author Daniel <daniel (at) danielsmedegaardbuus (dot) dk>
+ * @author Gabriel Sobrinho <gabriel (dot) sobrinho (at) gmail (dot) com>
+ * @author Rik Meijer <hello (at) rikmeijer (dot) nl>
+ */
+function array_merge_recursive_distinct(array $array1, array ...$arrays): array
+{
+    $merge = static function (array $merged, array ...$arrays) use (&$merge): array {
+        array_walk($arrays, static function (array $array) use ($merge, &$merged) {
+            foreach ($array as $key => $value) {
+                if (is_array($value) && isset($merged[$key]) && is_array($merged[$key])) {
+                    $merged [$key] = $merge($merged[$key], $value);
+                } else {
+                    $merged [$key] = $value;
+                }
+            }
+        });
+        return $merged;
+    };
+    return $merge($array1, ...$arrays);
+}
+
+function array_map_assoc(callable $callback, array $array, array ...$arrays): array
+{
+    $map = [];
+    foreach ($array as $key => $value) {
+        $map[$key] = $callback($value, ...(static function (string|int $key, array $arrays): array {
+            return array_map(static function (array $array) use ($key): mixed {
+                return $array[$key] ?? null;
+            }, $arrays);
+        })($key, $arrays));
+    }
+    return $map;
+}
 
 class Configuration
 {
     private static array $configs = [];
 
-    public static function open(string $root, string $section): array
+    public static function open(string $root, string $section, array $schema): array
     {
-        if (isset(self::$configs[$root]) === false) {
-            self::$configs[$root] = array_merge_recursive_distinct(self::openDefaults($root), self::openLocal($root));
+        if (array_key_exists($root, self::$configs) === false) {
+            self::$configs[$root] = array_merge_recursive_distinct([$section => []], self::openDefaults($root), self::openLocal($root));
         }
         if (array_key_exists($section, self::$configs[$root]) === false) {
-            return [];
+            self::$configs[$root][$section] = [];
         }
-        return self::$configs[$root][$section];
+        return self::validate($schema, self::$configs[$root][$section], ['%configuration-path%' => $root]);
     }
 
     private static function openDefaults(string $root): array
@@ -36,6 +90,28 @@ class Configuration
     private static function openLocal(string $root): array
     {
         return self::include($root . DIRECTORY_SEPARATOR . 'config.php');
+    }
+
+    private static function validate(array $schema, array $configuration, array $context): array
+    {
+        if (count($schema) === 0) {
+            return $configuration;
+        }
+        return array_map_assoc(function (array $schemaProperty, mixed $configurationProperty) use ($context) {
+            return call_user_func([__CLASS__, $schemaProperty[0]], $schemaProperty[1], $configurationProperty, $context);
+        }, $schema, $configuration);
+    }
+
+    /** @noinspection PhpUnusedPrivateMethodInspection */
+    private static function url(array $schemaProperty, mixed $configurationProperty, array $context): mixed
+    {
+        if ($configurationProperty !== null) {
+            return $configurationProperty;
+        }
+        if (array_key_exists('default', $schemaProperty)) {
+            return str_replace(array_keys($context), array_values($context), $schemaProperty['default']);
+        }
+        return null;
     }
 
 }
