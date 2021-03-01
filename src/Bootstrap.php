fc95675acdba63248581be1c3e69a98282d6bf7f
Rik Meijer <rik@pulledbits.org>
2021-03-01 12:18:11 +0100

    move context deduction to PHP class
    Signed-off-by: Rik Meijer <rik@pulledbits.org>

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -17,50 +17,6 @@ final class Bootstrap
         return Configuration::open($configurationPath, 'BOOTSTRAP', ['path' => Configuration::path('bootstrap'), 'functions-path' => Configuration::path('_f.php'), 'namespace' => Configuration::default(__NAMESPACE__ . '\\' . basename($configurationPath))]);
     }
 
-    private static function tokenFinder(array &$tokens): callable
-    {
-        return static function (mixed $id, ?int $maxTokenDistance = null) use (&$tokens): null|array|string {
-            while ($token = array_shift($tokens)) {
-                if ($maxTokenDistance > 0) {
-                    $maxTokenDistance--;
-                } elseif ($maxTokenDistance === 0) {
-                    return null;
-                }
-                if (is_string($id)) {
-                    if ($token === $id) {
-                        return $token;
-                    }
-                } elseif (is_int($id)) {
-                    if ($token[0] === $id) {
-                        return $token;
-                    }
-                }
-            }
-            return null;
-        };
-    }
-
-    private static function tokenCollector(array &$tokens): callable
-    {
-        return static function (mixed $id) use (&$tokens): null|array {
-            $buffer = [];
-            while ($token = array_shift($tokens)) {
-                if (is_string($id)) {
-                    if ($token === $id) {
-                        return $buffer;
-                    }
-                } elseif (is_int($id)) {
-                    if ($token[0] === $id) {
-                        return $buffer;
-                    }
-                }
-                $buffer[] = $token;
-            }
-            array_unshift($tokens, ...$buffer);
-            return null;
-        };
-    }
-
     public static function generate(string $configurationPath): void
     {
         $config = self::configuration($configurationPath);
@@ -68,50 +24,7 @@ final class Bootstrap
         $fp = fopen($config['functions-path'], 'wb');
         fwrite($fp, '<?php' . PHP_EOL);
         Resource::generate($config['path'], '', static function (string $resourceNSPath, string $resourcePath) use ($configurationPath, $resourcesNS, $fp) {
-            $resourceFileContents = file_get_contents($resourcePath);
-
-            $tokens = token_get_all($resourceFileContents, TOKEN_PARSE);
-
-            $findNextToken = self::tokenFinder($tokens);
-
-            $getUpToNextToken = self::tokenCollector($tokens);
-
-            $context = ['parameters' => ''];
-
-            while ($token = array_shift($tokens)) {
-                switch ($token[0]) {
-                    case T_NAMESPACE:
-                        $context['namespace'] = $findNextToken(T_NAME_QUALIFIED)[1];
-                        break;
-
-                    case T_RETURN:
-                        if ($findNextToken(T_FUNCTION, 3) !== null) {
-                            $findNextToken("(");
-                            foreach ($getUpToNextToken(")") as $parameterToken) {
-                                if (is_string($parameterToken)) {
-                                    $context['parameters'] .= $parameterToken;
-                                } else {
-                                    $context['parameters'] .= $parameterToken[1];
-                                }
-                            }
-
-
-                            $functionSignatureTokens = $getUpToNextToken('{');
-                            $functionSignatureTokenFinder = self::tokenFinder($functionSignatureTokens);
-                            if ($functionSignatureTokenFinder(":") !== null) {
-                                $context['returnType'] = ':';
-                                while ($functionSignatureToken = array_shift($functionSignatureTokens)) {
-                                    $context['returnType'] .= $functionSignatureToken[1];
-                                }
-                            }
-                        }
-                        break;
-
-                    default:
-                        // ignore
-                        break;
-                }
-            }
+            $context = PHP::deductContextFromFile($resourcePath);
 
             if (array_key_exists('namespace', $context)) {
                 $resourceNS = $context['namespace'];
@@ -121,11 +34,19 @@ final class Bootstrap
                 $resourceNS = $resourcesNS;
             }
 
+            $parameters = '';
+            if (array_key_exists('parameters', $context)) {
+                $parameters = $context['parameters'];
+            }
+
+            $returnType = '';
+            if (array_key_exists('returnType', $context)) {
+                $returnType = $context['returnType'];
+            }
 
             $configCode = '\\' . Configuration::class . '::open(' . PHP::export($configurationPath) . ', ' . PHP::export($resourceNSPath . basename($resourcePath, '.php')) . ', $schema);';
-
             fwrite($fp, PHP::function($resourceNS . '\\' . basename($resourcePath, '.php'), 'validate', 'array $schema', ': array', $configCode));
-            fwrite($fp, PHP::function($resourceNS, basename($resourcePath, '.php'), $context['parameters'], array_key_exists('returnType', $context) ? rtrim($context['returnType']) : '', '\\' . Resource::class . '::require(' . PHP::export($resourcePath) . ', static function(array $schema) {
+            fwrite($fp, PHP::function($resourceNS, basename($resourcePath, '.php'), $parameters, $returnType, '\\' . Resource::class . '::require(' . PHP::export($resourcePath) . ', static function(array $schema) {
                             return ' . $configCode . '
                         })(...func_get_args());'));
         });
--- src/PHP.php
+++ src/PHP.php
@@ -51,4 +51,91 @@ class PHP
         return var_export($variable, true);
     }
 
+    public static function deductContextFromFile(string $resourcePath): array
+    {
+        $resourceFileContents = file_get_contents($resourcePath);
+
+        $tokens = token_get_all($resourceFileContents, TOKEN_PARSE);
+
+        $findNextToken = self::tokenFinder($tokens);
+        $collectTokensUpTo = self::tokenCollector($tokens);
+
+        $context = [];
+        if ($findNextToken(T_NAMESPACE) !== null) {
+            $context['namespace'] = $findNextToken(T_NAME_QUALIFIED)[1];
+        }
+
+        if ($findNextToken(T_RETURN) === null) {
+            return $context;
+        }
+
+        $context['parameters'] = '';
+        if ($findNextToken(T_FUNCTION, 3) !== null) {
+            $findNextToken("(");
+            foreach ($collectTokensUpTo(")") as $parameterToken) {
+                if (is_string($parameterToken)) {
+                    $context['parameters'] .= $parameterToken;
+                } else {
+                    $context['parameters'] .= $parameterToken[1];
+                }
+            }
+
+
+            $functionSignatureTokens = $collectTokensUpTo('{');
+            $functionSignatureTokenFinder = self::tokenFinder($functionSignatureTokens);
+            if ($functionSignatureTokenFinder(":") !== null) {
+                $context['returnType'] = ':';
+                while ($functionSignatureToken = array_shift($functionSignatureTokens)) {
+                    $context['returnType'] .= $functionSignatureToken[1];
+                }
+                $context['returnType'] = rtrim($context['returnType']);
+            }
+        }
+        return $context;
+    }
+
+
+    private static function tokenFinder(array &$tokens): callable
+    {
+        return static function (mixed $id, ?int $maxTokenDistance = null) use (&$tokens): null|array|string {
+            while ($token = array_shift($tokens)) {
+                if ($maxTokenDistance > 0) {
+                    $maxTokenDistance--;
+                } elseif ($maxTokenDistance === 0) {
+                    return null;
+                }
+                if (is_string($id)) {
+                    if ($token === $id) {
+                        return $token;
+                    }
+                } elseif (is_int($id)) {
+                    if ($token[0] === $id) {
+                        return $token;
+                    }
+                }
+            }
+            return null;
+        };
+    }
+
+    private static function tokenCollector(array &$tokens): callable
+    {
+        return static function (mixed $id) use (&$tokens): null|array {
+            $buffer = [];
+            while ($token = array_shift($tokens)) {
+                if (is_string($id)) {
+                    if ($token === $id) {
+                        return $buffer;
+                    }
+                } elseif (is_int($id)) {
+                    if ($token[0] === $id) {
+                        return $buffer;
+                    }
+                }
+                $buffer[] = $token;
+            }
+            array_unshift($tokens, ...$buffer);
+            return null;
+        };
+    }
 }
