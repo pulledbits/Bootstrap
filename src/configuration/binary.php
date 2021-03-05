0299ef6039d0a035602b4666b1891d511c125a48
Rik Meijer <rik@pulledbits.org>
2021-03-05 20:51:02 +0100

    expect $out to be writable to a callback
    Signed-off-by: Rik Meijer <rik@pulledbits.org>

--- src/configuration/binary.php
+++ src/configuration/binary.php
@@ -8,29 +8,27 @@ return static function (string ...$defaultValue): callable {
         $binary = $pathValidator($value, $error, $context);
         if (file_exists($binary) === false) {
             $error('binary "' . $binary . '" does not exist');
-            return static function (): ?string {
-                return null;
+            return static function (): void {
             };
         }
-        return static function (string ...$arguments) use ($binary): ?string {
+        return static function (array $arguments, callable $out) use ($binary): void {
             $escapedArguments = array_map('escapeshellarg', $arguments);
             $command = '"' . $binary . '" ' . implode(' ', $escapedArguments);
             $process = proc_open($command, [STDIN,  // stdin is a pipe that the child will read from
                 ['pipe', 'w'],  // stdout is a pipe that the child will write to
                 STDERR // stderr is a file to write to
             ], $pipes);
-            if ($process === false) {
-                return null;
-            }
 
-            $output = '';
-            while (feof($pipes[1]) === false) {
-                $output .= fgets($pipes[1]);
+            if ($process !== false) {
+                while (feof($pipes[1]) === false) {
+                    $buffer = fgets($pipes[1]);
+                    if ($buffer !== false) {
+                        $out($buffer);
+                    }
+                }
+                fclose($pipes[1]);
+                proc_close($process);
             }
-            fclose($pipes[1]);
-            proc_close($process);
-
-            return $output;
         };
     };
 };
