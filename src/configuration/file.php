7e2d5284ee14dbfdf7fd3f12503b9d4d577f815f
Rik Meijer <rik@pulledbits.org>
2021-03-05 09:04:25 +0100

    $context is ignored from resource files
    Signed-off-by: Rik Meijer <rik@pulledbits.org>

--- src/configuration/file.php
+++ src/configuration/file.php
@@ -4,7 +4,7 @@ use Functional as F;
 use rikmeijer\Bootstrap\Configuration;
 
 return static function (string ...$defaultValue): callable {
-    $pathValidator = F\partial_right(F\partial_left([Configuration::class, "path"], implode(DIRECTORY_SEPARATOR, $defaultValue)), ["configuration-path" => __DIR__]);
+    $pathValidator = F\partial_left([Configuration::class, "path"], implode(DIRECTORY_SEPARATOR, $defaultValue));
     return static function (mixed $value, callable $error, array $context) use ($pathValidator) {
         $path = $pathValidator($value, $error, $context);
         return static function (string $mode) use ($path) {
--- src/configuration/path.php
+++ src/configuration/path.php
@@ -4,5 +4,5 @@ use Functional as F;
 use rikmeijer\Bootstrap\Configuration;
 
 return static function (string ...$defaultValue): callable {
-    return F\partial_right(F\partial_left([Configuration::class, "path"], implode(DIRECTORY_SEPARATOR, $defaultValue)), ["configuration-path" => __DIR__]);
+    return F\partial_left([Configuration::class, "path"], implode(DIRECTORY_SEPARATOR, $defaultValue));
 };
