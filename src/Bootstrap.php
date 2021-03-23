09c8f812622a5de739c849514b3eef074cda25b3
Rik Meijer <rik@rikmeijer.nl>
2021-03-23 09:26:40 +0100

    move configure function own file
    Signed-off-by: Rik Meijer <rik@rikmeijer.nl>

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -5,26 +5,6 @@ namespace rikmeijer\Bootstrap;
 use Functional as F;
 use Nette\PhpGenerator\GlobalFunction;
 
-function configure(callable $function, array $schema): callable
-{
-    $configurationPath = Bootstrap::configurationPath();
-    $resources = F\partial_left('Functional\\head', Bootstrap::resources($configurationPath));
-    $resourcePath = substr(debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS, 1)[0]["file"], 0, -4);
-    $resourceDir = preg_split('#[/\\\\]+#', $resourcePath);
-    $configSection = [];
-
-    do {
-        array_unshift($configSection, array_pop($resourceDir));
-        $path = $resources(F\partial_left(static function (string $resourceDir, string $path, string $resourcesPath): bool {
-            return fileinode($resourceDir) === fileinode($resourcesPath . DIRECTORY_SEPARATOR . $path);
-        }, implode(DIRECTORY_SEPARATOR, $resourceDir)));
-    } while ($path === null);
-    if ($path !== '') {
-        array_unshift($configSection, $path);
-    }
-    return F\partial_left($function, Configuration::validate($schema, $configurationPath, implode('/', $configSection)));
-}
-
 final class Bootstrap
 {
     public static function resources(string $configurationPath): array
---
+++ src/configure.php
@@ -0,0 +1,25 @@
+<?php declare(strict_types=1);
+
+namespace rikmeijer\Bootstrap;
+
+use Functional as F;
+
+function configure(callable $function, array $schema): callable
+{
+    $configurationPath = Bootstrap::configurationPath();
+    $resources = F\partial_left('Functional\\head', Bootstrap::resources($configurationPath));
+    $resourcePath = substr(debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS, 1)[0]["file"], 0, -4);
+    $resourceDir = preg_split('#[/\\\\]+#', $resourcePath);
+    $configSection = [];
+
+    do {
+        array_unshift($configSection, array_pop($resourceDir));
+        $path = $resources(F\partial_left(static function (string $resourceDir, string $path, string $resourcesPath): bool {
+            return fileinode($resourceDir) === fileinode($resourcesPath . DIRECTORY_SEPARATOR . $path);
+        }, implode(DIRECTORY_SEPARATOR, $resourceDir)));
+    } while ($path === null);
+    if ($path !== '') {
+        array_unshift($configSection, $path);
+    }
+    return F\partial_left($function, Configuration::validate($schema, $configurationPath, implode('/', $configSection)));
+}
