f9cdb8cde570b25f4b2cc650649874282b3e6b0c
Rik Meijer <rik@rikmeijer.nl>
2021-03-25 09:43:34 +0100

    $namespace parameter is only used internally
    Signed-off-by: Rik Meijer <rik@rikmeijer.nl>

--- src/resource/generate.php
+++ src/resource/generate.php
@@ -2,13 +2,19 @@
 
 namespace rikmeijer\Bootstrap\resource;
 
-function generate(string $resourcesPath, string $namespace, callable $writer): void
+use function Functional\partial_right;
+
+function generate(string $resourcesPath, callable $writer): void
 {
-    foreach (glob($resourcesPath . DIRECTORY_SEPARATOR . '*') as $resourceFilePath) {
-        if (is_dir($resourceFilePath)) {
-            generate($resourceFilePath, trim($namespace . '\\' . basename($resourceFilePath), '\\'), $writer);
-        } elseif (str_ends_with($resourceFilePath, '.php')) {
-            $writer($resourceFilePath, $namespace);
+    $generator = partial_right(static function (string $resourcesPath, string $namespace, callable $writer) use (&$generator): void {
+        foreach (glob($resourcesPath . DIRECTORY_SEPARATOR . '*') as $resourceFilePath) {
+            if (is_dir($resourceFilePath)) {
+                $generator($resourceFilePath, trim($namespace . '\\' . basename($resourceFilePath), '\\'));
+            } elseif (str_ends_with($resourceFilePath, '.php')) {
+                $writer($resourceFilePath, $namespace);
+            }
         }
-    }
+    }, $writer);
+
+    $generator($resourcesPath, '');
 }
