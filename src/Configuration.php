31beafe5fab07af5147189d1cf61d4e95fbbbe21
Rik Meijer <rik@rikmeijer.nl>
2021-03-26 14:32:15 +0100

    add NoReturn attribute
    Signed-off-by: Rik Meijer <rik@rikmeijer.nl>

--- src/Configuration.php
+++ src/Configuration.php
@@ -2,6 +2,7 @@
 
 namespace rikmeijer\Bootstrap;
 
+use JetBrains\PhpStorm\NoReturn;
 use function Functional\partial_left;
 use function trigger_error;
 
@@ -13,6 +14,7 @@ class Configuration
         return $validator($configuration[$property] ?? null, partial_left([__CLASS__, 'error'], $property));
     }
 
+    #[NoReturn]
     public static function error(string $property, string $message): void
     {
         trigger_error($property . ' ' . $message, E_USER_ERROR);
